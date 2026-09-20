2d85448e70f78a522d3c9c587bc8572c6994c777
RollingsDev <yorozuya4ever@gmail.com>
2026-09-20 20:44:15 -0300

    feat: configure isolated TFT meta feed

---
+++ config/tft-meta.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'meta_source' => 'https://tactics.tools/pt/team-compositions/all',
+    'items_source' => 'https://tactics.tools/pt/items',
+    'max_comps' => 6,
+    'max_items' => 8,
+    'minimum_item_games_display' => 0,
+];
