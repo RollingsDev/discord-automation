79224388facd8f029bdbbb554d2f5e18d4b6de64
RollingsDev <yorozuya4ever@gmail.com>
2026-09-20 18:59:50 -0300

    feat: configure Riot analytics players

---
+++ config/riot-players.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'region' => 'americas',
+    'platform' => 'br1',
+    'history_limit' => 100,
+    'backfill_per_game' => 8,
+
+    'players' => [
+        [
+            'id' => 'crow',
+            'game_name' => 'crow',
+            'tag_line' => 'GT1',
+        ],
+        [
+            'id' => 'nao-grita',
+            'game_name' => 'Não grita',
+            'tag_line' => 'grll',
+        ],
+        [
+            'id' => 'flafu',
+            'game_name' => 'flafu',
+            'tag_line' => 'ILY',
+        ],
+        [
+            'id' => 'lappush',
+            'game_name' => 'Lappush',
+            'tag_line' => 'br1',
+        ],
+        [
+            'id' => 'kingxds',
+            'game_name' => 'KingXds',
+            'tag_line' => 'br1',
+        ],
+    ],
+];
