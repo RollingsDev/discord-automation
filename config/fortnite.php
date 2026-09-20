6fababf8b0cadcfd684552534813b66213b87aa1
RollingsDev <yorozuya4ever@gmail.com>
2026-09-20 18:09:24 -0300

    fix: use free Fortnite news API

--- config/fortnite.php
+++ config/fortnite.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'state_key' => 'fortnite',
     'max_posts_per_run' => 6,
-    'news_url' => 'https://www.fortnite.com/news?lang=pt-BR',
+    'news_api_url' => 'https://fortnite-api.com/v2/news?language=pt-BR',
     'status_rss_url' => 'https://status.epicgames.com/history.rss',
 ];
